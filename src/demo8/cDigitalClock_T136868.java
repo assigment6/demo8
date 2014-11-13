6e18cf98b02c1ea5c8894d251178adf537d8e854
sadako7777 <luckybluestar7777@gmail.com>
2014-11-13 21:51:14 +0700

    12343

---
+++ src/demo8/cDigitalClock_T136868.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+package demo8;
+
+import javax.swing.JFrame;
+
+
+import java.awt.event.*;
+import javax.swing.*;
+import java.awt.*;
+import java.text.*;
+
+public class cDigitalClock_T136868 extends JFrame {
+	JLabel time;
+
+	cDigitalClock_T136868() {
+		setSize(300, 300);
+		setTitle("T136868- dong ho");
+		setLocation(150, 150);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new GridBagLayout());
+
+		time = new JLabel("");
+		time.setFont(new Font("SansSerif", Font.BOLD, 40));
+		time.setForeground(Color.BLACK);
+		add(time);
+		new Thread(new Runnable() {
+			public void run() {
+				try {
+					updateTime();
+				} catch (Exception ie) {
+				}
+			}
+		}).start();
+	}
+
+	public void updateTime() {
+		try {
+			while (true) {
+				time.setText(new SimpleDateFormat("hh:mm:ss ")
+						.format(new java.util.Date()));
+				Thread.currentThread().sleep(1000);
+			}
+		} catch (Exception e) {
+			System.out.println("Exception in Thread Sleep : " + e);
+		}
+	}
+
+	public static void main(String[] args) {
+		JFrame obj = new cDigitalClock_T136868();
+		obj.setVisible(true);
+	}
+}
